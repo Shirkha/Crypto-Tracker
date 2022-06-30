e57d60fe639733b133edea2778d341b4005987d6
Geek_dev <shirkha_ug@ece.nits.ac.in>
2022-07-01 03:02:11 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Cryptocurrency Tracker with React JS, Material UI and Chart JS
+
+
+## Demo
+#### Here is a working live demo : 
+
+
---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCA0rsihPrxRnH0ObROLq8KbHwTIkTBR04",
+  authDomain: "crypto-stats-c6629.firebaseapp.com",
+  projectId: "crypto-stats-c6629",
+  storageBucket: "crypto-stats-c6629.appspot.com",
+  messagingSenderId: "278444148874",
+  appId: "1:278444148874:web:fae6248885b9c9580bc573"
+};
+
+export default firebaseConfig;
